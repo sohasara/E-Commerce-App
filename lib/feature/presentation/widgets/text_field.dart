738110f4c0391dd11e13759619e0a73bba0b1347
Nadia Islam <sohasara853@gmail.com>
2024-10-02 11:38:19 +0600

    modify

--- lib/feature/presentation/widgets/text_field.dart
+++ lib/feature/presentation/widgets/text_field.dart
@@ -5,11 +5,17 @@ class MyTextFeild extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-          border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20),
-      )),
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 10.0,
+        right: 10,
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+        )),
+      ),
     );
   }
 }
