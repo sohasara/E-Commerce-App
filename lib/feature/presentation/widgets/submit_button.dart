4e49f61d54b108f73adfbdac85fb38daa95d5a1d
Nadia Islam <sohasara853@gmail.com>
2024-10-02 11:59:18 +0600

    modify

--- lib/feature/presentation/widgets/submit_button.dart
+++ lib/feature/presentation/widgets/submit_button.dart
@@ -5,16 +5,29 @@ class SubmitButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      width: 300,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        gradient: LinearGradient(
-          colors: [
-            Colors.pink.shade200,
-            Colors.pink.shade100,
-          ],
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: 55,
+        width: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              Colors.pink.shade200,
+              Colors.pink.shade300,
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Submit',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
     );
