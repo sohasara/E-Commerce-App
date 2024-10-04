3358dcbe9c93bfadad38f2fdec34daa2c226bcd8
Nadia Islam <sohasara853@gmail.com>
2024-10-04 19:39:58 +0600

    add navigation bar

---
+++ lib/feature/presentation/widgets/navigation_bar.dart
@@ -0,0 +1,28 @@
+import 'package:delivery_app_with_admin_pannel/feature/presentation/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyNavigationBar extends StatelessWidget {
+  const MyNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int currentindex = 0;
+    final List pages = [
+      const HomePage(),
+      const Text('category'),
+      const Text('profile'),
+    ];
+    return Scaffold(
+      body: pages[currentindex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentindex,
+        onTap: (value) {},
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        ],
+      ),
+    );
+  }
+}
