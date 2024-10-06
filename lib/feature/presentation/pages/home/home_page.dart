c3cd676a06c0271a4c37fc5c76df86abaf727963
Nadia Islam <sohasara853@gmail.com>
2024-10-06 08:55:31 +0600

    add text

--- lib/feature/presentation/pages/home/home_page.dart
+++ lib/feature/presentation/pages/home/home_page.dart
@@ -10,60 +10,71 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const MyTextF(),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 140,
-              width: 370,
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [
-                    Colors.pink,
-                    Colors.purple,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const MyTextF(),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 140,
+                width: 370,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [
+                      Colors.pink,
+                      Colors.purple,
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              ),
+              const Text(
+                ' Sales',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.pink,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 160,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 0.5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Container(
+                      height: 150,
+                      width: 160,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 0.5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
                   ],
                 ),
-                borderRadius: BorderRadius.circular(18),
               ),
-            ),
-            const Text(
-              ' Sales',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+              const Text(
+                'ALL ITEMS',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.pink,
+                ),
               ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Container(
-                    height: 150,
-                    width: 160,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 0.5),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Container(
-                    height: 150,
-                    width: 160,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 0.5),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
