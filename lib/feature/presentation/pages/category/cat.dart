1e42cff2299f852cf5460d8e96f646d277db8052
Nadia Islam <sohasara853@gmail.com>
2024-10-06 09:02:04 +0600

    add category text

--- lib/feature/presentation/pages/category/cat.dart
+++ lib/feature/presentation/pages/category/cat.dart
@@ -5,6 +5,22 @@ class CategoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            Text(
+              'Categories',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w600,
+                color: Colors.pink,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
