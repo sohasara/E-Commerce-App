54a14711bcbf515b29845da53c85fdfe5686002a
Nadia Islam <sohasara853@gmail.com>
2024-10-06 11:29:24 +0600

    add models

--- lib/feature/data/models/category_model.dart
+++ lib/feature/data/models/category_model.dart
@@ -1,7 +1,42 @@
 final categories = [
   {
     'id': '0',
-    'name': 'dress',
-    'iconurl': '',
+    'name': 'Dress',
+    'iconurl': 'assets/icons/dress.png',
+  },
+  {
+    'id': '1',
+    'name': 'Shoes',
+    'iconurl': 'assets/icons/shoe.png',
+  },
+  {
+    'id': '2',
+    'name': 'Bags',
+    'iconurl': 'assets/icons/bag.png',
+  },
+  {
+    'id': '3',
+    'name': 'Ornaments',
+    'iconurl': 'assets/icons/jewel.png',
+  },
+  {
+    'id': '4',
+    'name': 'Laptops',
+    'iconurl': 'assets/icons/laptop.png',
+  },
+  {
+    'id': '5',
+    'name': 'Phones',
+    'iconurl': 'assets/icons/phone.png',
+  },
+  {
+    'id': '6',
+    'name': 'Home Appliances',
+    'iconurl': 'assets/icons/appliances.png',
+  },
+  {
+    'id': '7',
+    'name': 'Home Decor',
+    'iconurl': 'assets/icons/home_dec.png',
   },
 ];
