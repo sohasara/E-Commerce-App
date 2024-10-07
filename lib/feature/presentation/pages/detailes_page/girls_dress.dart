cde5b79533e1d1f72b55516e19b29e03d6584af0
Nadia Islam <sohasara853@gmail.com>
2024-10-07 10:39:31 +0600

    add new file

---
+++ lib/feature/presentation/pages/detailes_page/girls_dress.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GirlsDress extends StatelessWidget {
+  const GirlsDress({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
