4ac841f8353f664ccde648b200b03e28d6793696
Nadia Islam <sohasara853@gmail.com>
2024-10-07 12:04:24 +0600

    add my grid

--- lib/feature/presentation/pages/detailes_page/girls_dress.dart
+++ lib/feature/presentation/pages/detailes_page/girls_dress.dart
@@ -1,3 +1,5 @@
+import 'package:delivery_app_with_admin_pannel/feature/presentation/widgets/my_grid.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -15,49 +17,54 @@ class GirlsDress extends ConsumerWidget {
       'Borkha',
     ];
 
-    // Accessing the index of the currently selected chip
     final selectedChipIndex = ref.watch(chipSelectionProvider);
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 50,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: girlDress.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ChoiceChip(
-                    label: Text(
-                      girlDress[index],
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 50,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: girlDress.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ChoiceChip(
+                      label: Text(
+                        girlDress[index],
+                      ),
+                      selected: selectedChipIndex == index,
+                      selectedColor: Colors.blue,
+                      backgroundColor: Colors.white,
+                      labelStyle: TextStyle(
+                        color: selectedChipIndex == index
+                            ? Colors.white
+                            : Colors.pink,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      showCheckmark: false,
+                      onSelected: (bool selected) {
+                        ref
+                            .read(chipSelectionProvider.notifier)
+                            .selectChip(index);
+                      },
                     ),
-                    selected: selectedChipIndex == index,
-                    selectedColor: Colors.blue,
-                    backgroundColor: Colors.white,
-                    labelStyle: TextStyle(
-                      color: selectedChipIndex == index
-                          ? Colors.white
-                          : Colors.pink,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    showCheckmark: false,
-                    onSelected: (bool selected) {
-                      ref
-                          .read(chipSelectionProvider.notifier)
-                          .selectChip(index);
-                    },
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+            const MyGrid()
+          ],
+        ),
       ),
     );
   }
