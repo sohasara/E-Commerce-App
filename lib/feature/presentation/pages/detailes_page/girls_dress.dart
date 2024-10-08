4e94383d5493acef994e756cea65b4f7d02317f8
Nadia Islam <sohasara853@gmail.com>
2024-10-08 18:46:51 +0600

    add chip container

--- lib/feature/presentation/pages/detailes_page/girls_dress.dart
+++ lib/feature/presentation/pages/detailes_page/girls_dress.dart
@@ -1,23 +1,19 @@
 import 'package:delivery_app_with_admin_pannel/feature/presentation/pages/detailes_page/my_grid.dart';
+import 'package:delivery_app_with_admin_pannel/feature/presentation/widgets/chip_container.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../riverpod/chip_select.dart';
-
-class GirlsDress extends ConsumerWidget {
+class GirlsDress extends StatelessWidget {
   const GirlsDress({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final List<String> girlDress = [
       'Bengali',
       'Pakistani',
       'Western',
       'Borkha',
     ];
-
-    final selectedChipIndex = ref.watch(chipSelectionProvider);
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -27,40 +23,16 @@ class GirlsDress extends ConsumerWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             SizedBox(
-              height: 50,
+              height: 55,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: girlDress.length,
                 itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ChoiceChip(
-                      label: Text(
-                        girlDress[index],
-                      ),
-                      selected: selectedChipIndex == index,
-                      selectedColor: Colors.blue,
-                      backgroundColor: Colors.white,
-                      labelStyle: TextStyle(
-                        color: selectedChipIndex == index
-                            ? Colors.white
-                            : Colors.pink,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      showCheckmark: false,
-                      onSelected: (bool selected) {
-                        ref
-                            .read(chipSelectionProvider.notifier)
-                            .selectChip(index);
-                      },
-                    ),
+                  return ChipContainer(
+                    name: girlDress[index],
                   );
                 },
               ),
-            ),
-            const SizedBox(
-              height: 10,
             ),
             const MyGrid()
           ],
