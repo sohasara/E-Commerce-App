9ff296b962e8fb40f97ec2f8f115de9cc00b54b6
Nadia Islam <sohasara853@gmail.com>
2024-10-08 11:15:20 +0600

    add boys dress model

--- lib/feature/data/models/boy_dress_model.dart
+++ lib/feature/data/models/boy_dress_model.dart
@@ -4,23 +4,23 @@ final boyDress = [
     'price': '3000',
   },
   {
-    'url': 'assets/images/boy1.webp',
+    'url': 'assets/images/boy2.jpeg',
     'price': '3000',
   },
   {
-    'url': 'assets/images/boy1.webp',
+    'url': 'assets/images/boy3.webp',
     'price': '3000',
   },
   {
-    'url': 'assets/images/boy1.webp',
+    'url': 'assets/images/boy4.jpg',
     'price': '3000',
   },
   {
-    'url': 'assets/images/boy1.webp',
+    'url': 'assets/images/boy5.webp',
     'price': '3000',
   },
   {
-    'url': 'assets/images/boy1.webp',
+    'url': 'assets/images/boy6.webp',
     'price': '3000',
   },
 ];
