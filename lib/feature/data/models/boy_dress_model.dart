567d54e741f43e065ad6357c09095097238b7c70
Nadia Islam <sohasara853@gmail.com>
2024-10-08 11:14:18 +0600

    add images in assets

--- lib/feature/data/models/boy_dress_model.dart
+++ lib/feature/data/models/boy_dress_model.dart
@@ -1,1 +1,26 @@
-final boyDress = [{}];
+final boyDress = [
+  {
+    'url': 'assets/images/boy1.webp',
+    'price': '3000',
+  },
+  {
+    'url': 'assets/images/boy1.webp',
+    'price': '3000',
+  },
+  {
+    'url': 'assets/images/boy1.webp',
+    'price': '3000',
+  },
+  {
+    'url': 'assets/images/boy1.webp',
+    'price': '3000',
+  },
+  {
+    'url': 'assets/images/boy1.webp',
+    'price': '3000',
+  },
+  {
+    'url': 'assets/images/boy1.webp',
+    'price': '3000',
+  },
+];
