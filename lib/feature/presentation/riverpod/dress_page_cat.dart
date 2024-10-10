e434ec8e0167f452e1dc053b151100da2de9aa77
Nadia Islam <sohasara853@gmail.com>
2024-10-10 21:56:14 +0600

    modify

--- lib/feature/presentation/riverpod/dress_page_cat.dart
+++ lib/feature/presentation/riverpod/dress_page_cat.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DressCategoryNotifier extends StateNotifier<String> {
-  DressCategoryNotifier(super.state);
+class GirlDressCategoryNotifier extends StateNotifier<String> {
+  GirlDressCategoryNotifier(super.state);
   void selectCategory(String category) {
     state = category;
   }
 }
+
+final GirlDressCategoryProvider =
+    StateNotifierProvider<GirlDressCategoryNotifier, String>((ref) {
+  return GirlDressCategoryNotifier('All');
+});
