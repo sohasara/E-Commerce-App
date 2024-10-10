a13dbf16b194a504ac131e06100b4dd004e8f61f
Nadia Islam <sohasara853@gmail.com>
2024-10-10 21:32:43 +0600

    add function

--- lib/feature/presentation/riverpod/dress_page_cat.dart
+++ lib/feature/presentation/riverpod/dress_page_cat.dart
@@ -2,4 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DressCategoryNotifier extends StateNotifier<String> {
   DressCategoryNotifier(super.state);
+  void selectCategory(String category) {
+    state = category;
+  }
 }
