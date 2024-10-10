cd8e5e39b5a486e3fb04398ff614cec12073fcfb
Nadia Islam <sohasara853@gmail.com>
2024-10-10 11:56:01 +0600

    add chips

--- lib/feature/presentation/pages/category/shoe_page.dart
+++ lib/feature/presentation/pages/category/shoe_page.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app_with_admin_pannel/feature/presentation/widgets/chip_container.dart';
 import 'package:flutter/material.dart';
 
 class ShoePage extends StatelessWidget {
@@ -5,9 +6,24 @@ class ShoePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final shoesname = ['slippers', 'Snekers'];
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('ShoesPage'),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 55,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: shoesname.length,
+                itemBuilder: (context, index) {
+                  return ChipContainer(name: shoesname[index]);
+                }),
+          ),
+        ],
       ),
     );
   }
