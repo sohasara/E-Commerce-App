4350d922d8562c14b42935a15ba4db2ef3e19afc
Nadia Islam <sohasara853@gmail.com>
2024-10-10 11:36:49 +0600

    add page new page and class

---
+++ lib/feature/presentation/pages/category/laptop.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LaptopPage extends StatelessWidget {
+  const LaptopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/feature/presentation/pages/category/ornaments.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrnamentsPage extends StatelessWidget {
+  const OrnamentsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
