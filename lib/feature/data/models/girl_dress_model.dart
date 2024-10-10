06ac83c6cf1e5ebddbda1ef234fd753a934c7d8d
Nadia Islam <sohasara853@gmail.com>
2024-10-10 19:53:58 +0600

    add category

--- lib/feature/data/models/girl_dress_model.dart
+++ lib/feature/data/models/girl_dress_model.dart
@@ -1,25 +1,31 @@
 final girldress = [
   {
+    'category': 'bengali',
     'url': 'assets/images/dress1.webp',
     'price': '5000',
   },
   {
+    'category': 'bengali',
     'url': 'assets/images/dress2.webp',
     'price': '2000',
   },
   {
+    'category': 'bengali',
     'url': 'assets/images/dress3.jpeg',
     'price': '3000',
   },
   {
+    'category': 'bengali',
     'url': 'assets/images/dress4.jpeg',
     'price': '3500',
   },
   {
+    'category': 'bengali',
     'url': 'assets/images/dress5.webp',
     'price': '400',
   },
   {
+    'category': 'bengali',
     'url': 'assets/images/dress6.jpg',
     'price': '700',
   },
