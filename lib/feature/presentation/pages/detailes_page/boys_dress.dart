6cb42ca8bb7ec741d03c8e7d9815756a6dd07bf9
Nadia Islam <sohasara853@gmail.com>
2024-10-11 20:55:03 +0600

    make consumer widget

--- lib/feature/presentation/pages/detailes_page/boys_dress.dart
+++ lib/feature/presentation/pages/detailes_page/boys_dress.dart
@@ -2,13 +2,15 @@ import 'package:delivery_app_with_admin_pannel/feature/presentation/pages/detail
 
 import 'package:delivery_app_with_admin_pannel/feature/presentation/widgets/chip_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class BoysDress extends StatelessWidget {
+class BoysDress extends ConsumerWidget {
   const BoysDress({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final List<String> boysCat = [
+      'All',
       'Bengali',
       'Pant',
       'Shirt',
