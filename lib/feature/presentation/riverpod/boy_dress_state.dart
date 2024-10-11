51b2cd14a199a82bbf18e2df5b637e9036107674
Nadia Islam <sohasara853@gmail.com>
2024-10-11 20:53:23 +0600

    add dress state

--- lib/feature/presentation/riverpod/boy_dress_state.dart
+++ lib/feature/presentation/riverpod/boy_dress_state.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app_with_admin_pannel/feature/data/models/boy_dress_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class BoyChipState extends StateNotifier<String> {
@@ -9,4 +10,14 @@ class BoyChipState extends StateNotifier<String> {
 
 final boychipProvider = StateNotifierProvider<BoyChipState, String>((ref) {
   return BoyChipState('All');
+});
+
+final boydressprovider = Provider<List<Map<String, String>>>((ref) {
+  final selectedCat = ref.watch(boychipProvider);
+  if (selectedCat == 'All') {
+    return boyDress;
+  }
+  return boyDress
+      .where((boydres) => boydres['category']! == selectedCat)
+      .toList();
 });
