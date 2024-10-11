be5d54127c8033bd23021a0b89a4457c194c0adf
Nadia Islam <sohasara853@gmail.com>
2024-10-11 19:23:42 +0600

    modify model

--- lib/feature/data/models/boy_dress_model.dart
+++ lib/feature/data/models/boy_dress_model.dart
@@ -1,25 +1,31 @@
 final boyDress = [
   {
+    'category': 'Shirt',
     'url': 'assets/images/boy1.webp',
     'price': '3000',
   },
   {
+    'category': 'Pant',
     'url': 'assets/images/boy2.jpeg',
     'price': '3000',
   },
   {
+    'category': 'T-Shirt',
     'url': 'assets/images/boy3.webp',
     'price': '3000',
   },
   {
+    'category': 'Shirt',
     'url': 'assets/images/boy4.jpg',
     'price': '3000',
   },
   {
+    'category': 'Bengali',
     'url': 'assets/images/boy5.webp',
     'price': '3000',
   },
   {
+    'category': 'Shirt',
     'url': 'assets/images/boy6.webp',
     'price': '3000',
   },
