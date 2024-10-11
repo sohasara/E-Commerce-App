defde186c04984f5dd28cce43e4c083ea8b2231b
Nadia Islam <sohasara853@gmail.com>
2024-10-11 19:10:37 +0600

    modify

--- lib/feature/presentation/pages/detailes_page/girls_dress.dart
+++ lib/feature/presentation/pages/detailes_page/girls_dress.dart
@@ -1,48 +1,3 @@
-// import 'package:delivery_app_with_admin_pannel/feature/presentation/pages/detailes_page/my_grid.dart';
-// import 'package:delivery_app_with_admin_pannel/feature/presentation/widgets/chip_container.dart';
-
-// import 'package:flutter/material.dart';
-
-// class GirlsDress extends StatelessWidget {
-//   const GirlsDress({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final List<String> girlDress = [
-//       'All',
-//       'Bengali',
-//       'Pakistani',
-//       'Western',
-//       'Borkha',
-//     ];
-
-//     return Padding(
-//       padding: const EdgeInsets.all(8.0),
-//       child: SingleChildScrollView(
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           children: [
-//             SizedBox(
-//               height: 55,
-//               child: ListView.builder(
-//                 scrollDirection: Axis.horizontal,
-//                 itemCount: girlDress.length,
-//                 itemBuilder: (context, index) {
-//                   return ChipContainer(
-//                     name: girlDress[index],
-//                   );
-//                 },
-//               ),
-//             ),
-//             const MyGrid(),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:delivery_app_with_admin_pannel/feature/presentation/riverpod/girl_dress_page_cat.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -80,15 +35,13 @@ class GirlsDress extends ConsumerWidget {
                 itemBuilder: (context, index) {
                   return GestureDetector(
                     onTap: () {
-                      // Update selected category when a chip is tapped
                       ref
                           .read(girlDressCategoryProvider.notifier)
                           .selectCategory(girlDress[index]);
                     },
                     child: ChipContainer(
                       name: girlDress[index],
-                      isSelected: selectedCategory ==
-                          girlDress[index], // Highlight selected chip
+                      isSelected: selectedCategory == girlDress[index],
                     ),
                   );
                 },
@@ -107,11 +60,7 @@ class GirlsDress extends ConsumerWidget {
               itemCount: filteredDresses.length,
               itemBuilder: (context, index) {
                 final dress = filteredDresses[index];
-                return Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 0.5),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                return Card(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
