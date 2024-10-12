068636e0a6550faf8cca90b55959bbb08e3456e5
Nadia Islam <sohasara853@gmail.com>
2024-10-12 13:16:21 +0600

    add ornaments model

---
+++ lib/feature/data/models/ornaments_model.dart
@@ -0,0 +1,32 @@
+final ornamentsModel = [
+  {
+    'category': 'Earrings',
+    'url': 'assets/images/earring1.webp',
+    'price': '1000',
+  },
+  {
+    'category': 'Earrings',
+    'url': 'assets/images/earring2.jpg',
+    'price': '1000',
+  },
+  {
+    'category': 'Necklace',
+    'url': 'assets/images/neck1.jpg',
+    'price': '1000',
+  },
+  {
+    'category': 'Necklace',
+    'url': 'assets/images/nack2.webp',
+    'price': '1000',
+  },
+  {
+    'category': 'Rings',
+    'url': 'assets/images/ring2.jpg',
+    'price': '1000',
+  },
+  {
+    'category': 'Rings',
+    'url': 'assets/images/ring1.jpeg',
+    'price': '1000',
+  },
+];
