c14451481cd220b0494f346dc21872eaedf535be
Nadia Islam <sohasara853@gmail.com>
2024-10-12 13:08:41 +0600

    add bag models

--- lib/feature/data/models/bag_model.dart
+++ lib/feature/data/models/bag_model.dart
@@ -1,3 +1,32 @@
 final bagsModel = [
-  {},
+  {
+    'category': 'BagPacks',
+    'url': 'assets/images/bag4.jpg',
+    'price': '1000',
+  },
+  {
+    'category': 'BagPacks',
+    'url': 'assets/images/bag5.webp',
+    'price': '1000',
+  },
+  {
+    'category': 'Side Bags',
+    'url': 'assets/images/bag6.webp',
+    'price': '1000',
+  },
+  {
+    'category': 'Shoulder Bags',
+    'url': 'assets/images/bag3.webp',
+    'price': '1000',
+  },
+  {
+    'category': 'Side Bags',
+    'url': 'assets/images/bag2.jpg',
+    'price': '1000',
+  },
+  {
+    'category': 'BagPacks',
+    'url': 'assets/images/bag1.jpg',
+    'price': '1000',
+  },
 ];
