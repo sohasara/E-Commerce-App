8f6a257585cb6f87a413da041fdd52eb77219847
Nadia Islam <sohasara853@gmail.com>
2024-10-12 13:28:58 +0600

    add home decor model

---
+++ lib/feature/data/models/decor_model.dart
@@ -0,0 +1,26 @@
+final decorModel = [
+  {
+    'url': 'assets/images/decor1.jpeg',
+    'price': '1000',
+  },
+  {
+    'url': 'assets/images/decor2.jpg',
+    'price': '1000',
+  },
+  {
+    'url': 'assets/images/decor3.jpg',
+    'price': '1000',
+  },
+  {
+    'url': 'assets/images/decor4.webp',
+    'price': '1000',
+  },
+  {
+    'url': 'assets/images/decor5.webp',
+    'price': '1000',
+  },
+  {
+    'url': 'assets/images/decor6.webp',
+    'price': '1000',
+  },
+];
