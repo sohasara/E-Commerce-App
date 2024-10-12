4c254f759b23742d7d59923b3b656b478ea346f3
Nadia Islam <sohasara853@gmail.com>
2024-10-12 13:23:30 +0600

    add appliances model

---
+++ lib/feature/data/models/appliances_model.dart
@@ -0,0 +1,32 @@
+final ornamentsModel = [
+  {
+    'category': 'Fridge',
+    'url': 'assets/images/homeAppl1.jpeg',
+    'price': '1000',
+  },
+  {
+    'category': 'Fridge',
+    'url': 'assets/images/homeAppli2.jpeg',
+    'price': '1000',
+  },
+  {
+    'category': 'Blender',
+    'url': 'assets/images/homeAppli3.jpg',
+    'price': '1000',
+  },
+  {
+    'category': 'Blender',
+    'url': 'assets/images/homeAppli4.jpg',
+    'price': '1000',
+  },
+  {
+    'category': 'Washing Mechine',
+    'url': 'assets/images/homeAppli5.jpeg',
+    'price': '1000',
+  },
+  {
+    'category': 'Washing Mechine',
+    'url': 'assets/images/homeAppli6.png',
+    'price': '1000',
+  },
+];
