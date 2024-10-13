07ab0423f3cd806712059f882367f3ece8600651
Nadia Islam <sohasara853@gmail.com>
2024-10-13 13:08:43 +0600

    add bag provider

---
+++ lib/feature/presentation/riverpod/bag_state.dart
@@ -0,0 +1,12 @@
+import 'package:delivery_app_with_admin_pannel/feature/data/models/bag_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'index_select_state.dart';
+
+final bagprovider = Provider<List<Map<String, String>>>((ref) {
+  final selectedCat = ref.watch(chipProvider);
+  if (selectedCat == 'All') {
+    return bagsModel;
+  }
+  return bagsModel.where((bag) => bag['category']! == selectedCat).toList();
+});
