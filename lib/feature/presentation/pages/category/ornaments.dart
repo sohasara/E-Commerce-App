76eba26afc191acef3d651e84330454233860962
Nadia Islam <sohasara853@gmail.com>
2024-10-13 13:04:18 +0600

    rename

--- lib/feature/presentation/pages/category/ornaments.dart
+++ lib/feature/presentation/pages/category/ornaments.dart
@@ -1,4 +1,4 @@
-import 'package:delivery_app_with_admin_pannel/feature/presentation/riverpod/girl_dress_page_cat.dart';
+import 'package:delivery_app_with_admin_pannel/feature/presentation/riverpod/girl_dress_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
