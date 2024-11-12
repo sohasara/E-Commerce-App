171607f988581bf7f46b2aae0693e6064a13cc2b
Nadia Islam <sohasara853@gmail.com>
2024-11-12 08:59:40 +0600

    add item cart

--- lib/feature/presentation/pages/cart/item_cart.dart
+++ lib/feature/presentation/pages/cart/item_cart.dart
@@ -24,72 +24,82 @@ class Cart extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 10.0),
-            child: SizedBox(
-              height: 160,
-              width: 400,
-              child: Card(
-                elevation: 2,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        SizedBox(
-                          height: 145,
-                          width: 130,
-                          child: Card(
-                            elevation: 2,
-                            child: Image.asset(
-                              "assets/images/bag1.jpg",
-                              fit: BoxFit.cover,
-                            ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                  itemCount: 7,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: SizedBox(
+                        height: 160,
+                        width: 400,
+                        child: Card(
+                          elevation: 2,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  SizedBox(
+                                    height: 145,
+                                    width: 130,
+                                    child: Card(
+                                      elevation: 2,
+                                      child: Image.asset(
+                                        "assets/images/bag1.jpg",
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    "Vintage Bag ",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.add_rounded,
+                                        size: 30,
+                                        color: Colors.green,
+                                      ),
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        "0",
+                                        style: TextStyle(
+                                          fontSize: 23,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      Icon(
+                                        Icons.remove,
+                                        color: Colors.red,
+                                        size: 30,
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
                           ),
                         ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "Vintage Bag ",
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.add_rounded,
-                              size: 30,
-                              color: Colors.green,
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              "0",
-                              style: TextStyle(
-                                fontSize: 23,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Icon(
-                              Icons.remove,
-                              color: Colors.red,
-                              size: 30,
-                            )
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
+                      ),
+                    );
+                  }),
             ),
           ),
         ],
