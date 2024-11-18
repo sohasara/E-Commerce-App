e9e055f83a88b9b4e2b3c3440a4c36fe2127852e
Nadia Islam <sohasara853@gmail.com>
2024-11-18 13:21:43 +0600

    add text

--- lib/feature/presentation/pages/profile/profile_page.dart
+++ lib/feature/presentation/pages/profile/profile_page.dart
@@ -6,30 +6,54 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple[100],
+      backgroundColor: Colors.purple[200],
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
-            Text(
-              "Profile",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-            Center(
-              child: Container(
-                height: 150,
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.circular(100),
+            // Text(
+            //   "Profile",
+            //   style: TextStyle(
+            //     fontSize: 30,
+            //     fontWeight: FontWeight.w900,
+            //   ),
+            // ),
+            Column(
+              children: [
+                Center(
+                  child: Container(
+                    height: 130,
+                    width: 130,
+                    decoration: BoxDecoration(
+                      color: Colors.purple[300],
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Icon(
+                      Icons.person_3_rounded,
+                      size: 100,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-                child: Icon(
-                  Icons.person_3_rounded,
-                  size: 100,
-                  color: Colors.white,
+                Text(
+                  "Nadia Islam Rupa",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            // const SizedBox(
+            //   height: 30,
+            // ),
+            Container(
+              height: 430,
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(80),
+                  topRight: Radius.circular(80),
                 ),
               ),
             )
