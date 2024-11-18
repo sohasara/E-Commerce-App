99b1fc231a66ffccd8ab7252318268ed0710abb1
Nadia Islam <sohasara853@gmail.com>
2024-11-18 11:22:53 +0600

    move file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:delivery_app_with_admin_pannel/feature/presentation/pages/log_in.dart';
+import 'package:delivery_app_with_admin_pannel/auth/log_in.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
