62326b412d34b78c3b7607debd3d4128a6f33b3a
Nadia Islam <sohasara853@gmail.com>
2024-11-19 09:46:51 +0600

    change radius

--- lib/feature/presentation/pages/profile/profile_page.dart
+++ lib/feature/presentation/pages/profile/profile_page.dart
@@ -52,8 +52,8 @@ class ProfilePage extends StatelessWidget {
             decoration: BoxDecoration(
               color: Colors.purple,
               borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(50),
-                topRight: Radius.circular(50),
+                topLeft: Radius.circular(70),
+                topRight: Radius.circular(70),
               ),
             ),
           ),
