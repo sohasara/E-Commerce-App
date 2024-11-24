21796fab9d3905eb550f64b63fea22140d79fe70
Nadia Islam <sohasara853@gmail.com>
2024-11-24 11:23:23 +0600

    add image

--- lib/feature/presentation/pages/detailes/item_detailes.dart
+++ lib/feature/presentation/pages/detailes/item_detailes.dart
@@ -6,7 +6,27 @@ class DetailedItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(
+          "Wallmates",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Image.asset(
+            "assets/images/decor2.jpg",
+            height: 330,
+            width: double.infinity,
+            fit: BoxFit.fitWidth,
+          ),
+          Text("data"),
+        ],
+      ),
     );
   }
 }
